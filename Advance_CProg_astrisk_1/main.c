3b629f7fd3231088cd0692f23ca295b2df58992e
Joseph Akinboyede <josephakinboyede16@gmail.com>
2023-08-18 00:18:49 +0200

    updated debug code

--- Advance_CProg_astrisk_1/main.c
+++ Advance_CProg_astrisk_1/main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void challenge(void){
+    int num, x, *numb;
+    printf("Enter a integer number: \n");
+    scanf("%d", &num);
+    printf("This is the integer number you entered number %d: \n", num);
+    numb = &num;
+    for(x=-1; x<10; x++){
+        printf("This is the number %d: \n", *numb); //*numb is getting the value at the location
+        ++*numb; //this increment the value in the pointer
+        //*numb++ increment the address to something else
+    }
+
+}
 int main()
 {
     char *string = "greetings from GBTechworld\n";
@@ -13,17 +26,4 @@ int main()
 
     challenge();
     return 0;
-}
-void challenge(void){
-    int num, x, *numb;
-    printf("Enter a integer number: \n");
-    scanf("%d", &num);
-    printf("This is the integer number you entered number %d: \n", num);
-    numb = &num;
-    for(x=0; x<10; x++){
-        printf("This is the number %d: \n", *numb); //*numb is getting the value at the location
-        ++*numb; //this increment the value in the pointer
-        //*numb++ increment the address to something else
-    }
-
 }
